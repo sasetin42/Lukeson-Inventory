7176623817f7b1feba6693d24358b66982d1854d
Cesar Trongcoso Jr. <cesartrongcoso@gmail.com>
2025-08-15 00:08:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/inventory/products-tab.tsx
+++ src/components/inventory/products-tab.tsx
@@ -17,7 +17,7 @@ export default function ProductsTab() {
         <div className="flex items-center gap-2">
             <div className="relative">
                 <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-                <Input placeholder="Search products by name or SKU..." className="pl-9" />
+                <Input placeholder="Search products by name or SKU..." className="pl-9 w-80" />
             </div>
             <Select>
                 <SelectTrigger className="w-[180px]">
