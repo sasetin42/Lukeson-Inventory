8ff82e3e954a158b242e6e6535a977e52055bf20
Cesar Trongcoso Jr. <cesartrongcoso@gmail.com>
2025-08-15 16:50:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -290,13 +290,15 @@ export default function InventoryPage() {
       />
 
       {/* Add Category Dialog */}
-      <AddCategoryDialog
-          open={isAddCategoryOpen}
-          onOpenChange={setAddCategoryOpen}
-          onConfirm={handleAddCategory}
-          isSubmitting={isSubmitting}
-          existingCategories={categories}
-      />
+      {isAddCategoryOpen && (
+        <AddCategoryDialog
+            open={isAddCategoryOpen}
+            onOpenChange={setAddCategoryOpen}
+            onConfirm={handleAddCategory}
+            isSubmitting={isSubmitting}
+            existingCategories={categories}
+        />
+      )}
     </div>
   );
 }
